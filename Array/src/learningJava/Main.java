90a1cdd8c3a32598b1686f47e4b8293432b56dd6
noky <nokkky_brave@163.com>
2020-01-15 20:57:10 -0800

    Array

---
+++ Array/src/learningJava/Main.java
@@ -0,0 +1,16 @@
+package learningJava;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int[] myVariable ;
+        myVariable = new int[10]; // 10 int variables
+        myVariable[5] = 50; // 6 variable's value is 50
+        double[] myDoubleArray = new double[10];
+
+        System.out.println(myVariable[5]);
+    }
+}
+
+// Array
