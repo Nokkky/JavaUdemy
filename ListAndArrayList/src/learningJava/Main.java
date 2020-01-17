eef44aad7657bc37351a593173e832c266143a23
noky <nokkky_brave@163.com>
2020-01-16 21:33:33 -0800

    Finish ArrayList

---
+++ ListAndArrayList/src/learningJava/Main.java
@@ -0,0 +1,34 @@
+package learningJava;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+	// write your code here
+        //ArrayList
+        ArrayList<String> myArrayList = new ArrayList<String>();
+        int position = 10;
+        myArrayList.get(0);
+
+
+    }
+}
+//ArrayList
+// -- Can only use object/class can not use primitive type
+// -- myArrayList.add(item)
+// -- myArrayList.size()
+// -- myArrayList.get(position)
+// -- myArrayList.set(position, newItem)
+// -- myArrayList.contains(searchObject) return true if contain.
+// -- myArrayList.indexOf(searchObject)
+// -- myArrayList.remove(position)
+// -- myArrayList.allAll(Collection) // add at the end of the ArrayList
+// -- myArrayList.addAll(position, Collection)
+// -- myArrayList.toArray()
+// -- ArrayList<String> nextArray = new ArrayList<String>(myArrayList); // copy and construct a new ArrayList.
+//
+//
+//
